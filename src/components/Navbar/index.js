4844be48da3b72113f84a871f28e2eb2dd76bc87
Paulo Guilherme Bacelar Andrade <paulobacelara@gmail.com>
2021-09-19 17:44:05 -0300

    Created JSX for Navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,7 +1,24 @@
+import "./styles.css";
 import React from "react";
+import logo from "../../img/logo.svg";
 
 const index = () => {
-  return <div></div>;
+  return (
+    <nav className="nav">
+      <div className="nav__main">
+        <h1 className="nav__title">Covid-19 Dashboard</h1>&emsp;
+        <img src={logo} alt="Coronavirus" className="nav__logo" />
+      </div>
+
+      <div className="nav__menu">
+        <ul className="nav__list">
+          <li className="nav__list-item">Cases</li>
+          <li className="nav__list-item">Deaths</li>
+          <li className="nav__list-item">Vaccination</li>
+        </ul>
+      </div>
+    </nav>
+  );
 };
 
 export default index;
