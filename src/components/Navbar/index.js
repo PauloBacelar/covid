f15ba7b8cc72eb16228ec0f9fdec13cde327b85c
Paulo Guilherme Bacelar Andrade <paulobacelara@gmail.com>
2021-09-20 16:53:37 -0300

    Changed toggle navbar logic

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,8 +1,11 @@
 import "./styles.css";
 import React from "react";
 import logo from "../../img/logo.svg";
+import { useState } from "react";
 
-const index = () => {
+const Navbar = () => {
+  const [showNav, setShowNav] = useState(false);
+
   return (
     <nav className="nav">
       <div className="nav__main">
@@ -10,15 +13,28 @@ const index = () => {
         <img src={logo} alt="Coronavirus" className="nav__logo" />
       </div>
 
-      <div className="nav__menu">
-        <ul className="nav__list">
-          <li className="nav__list-item">Cases</li>
-          <li className="nav__list-item">Deaths</li>
-          <li className="nav__list-item">Vaccination</li>
-        </ul>
+      <ul className={`nav__list ${showNav ? "nav__list-active" : ""}`}>
+        <li className="nav__list-item">Cases</li>
+        <li className="nav__list-item">Deaths</li>
+        <li className="nav__list-item">Vaccination</li>
+      </ul>
+
+      <div
+        className={`nav__burger ${showNav ? "nav__burger-active" : ""}`}
+        onClick={() => setShowNav(!showNav)}
+      >
+        <div
+          className={`nav__burger-1 ${showNav ? "nav__burger-1-active" : ""}`}
+        ></div>
+        <div
+          className={`nav__burger-2 ${showNav ? "nav__burger-2-active" : ""}`}
+        ></div>
+        <div
+          className={`nav__burger-3 ${showNav ? "nav__burger-3-active" : ""}`}
+        ></div>
       </div>
     </nav>
   );
 };
 
-export default index;
+export default Navbar;
