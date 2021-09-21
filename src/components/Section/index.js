2a40bb193e6cee53b06bcef89502a09a4ecb32c4
Paulo Guilherme Bacelar Andrade <paulobacelara@gmail.com>
2021-09-21 17:36:17 -0300

    Created markup for Section

--- src/components/Section/index.js
+++ src/components/Section/index.js
@@ -1,7 +1,12 @@
 import React from "react";
+import "./styles.css";
 
-const Section = () => {
-  return <section></section>;
+const Section = ({ title }) => {
+  return (
+    <section className="section">
+      <h2 className="section__title">{title}</h2>
+    </section>
+  );
 };
 
 export default Section;
