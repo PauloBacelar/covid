2098ab1e8f49e0f6b60e6889c252fa8002188b75
Paulo Guilherme Bacelar Andrade <paulobacelara@gmail.com>
2021-09-21 18:11:55 -0300

    Created markup for grid items

--- src/components/CountriesGrid/index.js
+++ src/components/CountriesGrid/index.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from "react";
+import "./styles.css";
 
-const CountriesGrid = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+const CountriesGrid = ({ data, flags }) => {
+  console.log(data);
+  return (
+    <div className="section__grid">
+      {data.map((country) => {
+        return (
+          <div className="section__grid-item">
+            <h3>{country.country}</h3>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
 
-export default CountriesGrid
+export default CountriesGrid;
