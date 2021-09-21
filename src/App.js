7130afdcb3d43f4c3b379a309177a400f8cb0c75
Paulo Guilherme Bacelar Andrade <paulobacelara@gmail.com>
2021-09-21 16:57:17 -0300

    Get api.js data and assign it to hook

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
 import Navbar from "./components/Navbar";
+import { useEffect, useState } from "react";
+import data from "./api/api";
 
 function App() {
+  const [timeline, setTimeline] = useState([]);
+
+  useEffect(() => {
+    const loadData = async () => {
+      let timeline = await data.getCountriesTimeline();
+      setTimeline(timeline);
+      console.log(timeline);
+    };
+
+    loadData();
+  }, []);
+
   return (
     <>
       <header>
