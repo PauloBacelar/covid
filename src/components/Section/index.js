a1883405f64de03834a692f59a47bb50a3e8867d
Paulo Guilherme Bacelar Andrade <paulobacelara@gmail.com>
2021-09-21 18:12:18 -0300

    Showing CountriesGrid component

--- src/components/Section/index.js
+++ src/components/Section/index.js
@@ -1,10 +1,13 @@
 import React from "react";
+import CountriesGrid from "../CountriesGrid";
 import "./styles.css";
 
-const Section = ({ title }) => {
+const Section = ({ title, data, flags }) => {
   return (
     <section className="section">
       <h2 className="section__title">{title}</h2>
+
+      <CountriesGrid data={data} flags={flags} />
     </section>
   );
 };
