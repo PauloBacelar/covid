054b20f58cc5f258ba737ce6388f318b2f0a3da6
Paulo Guilherme Bacelar Andrade <paulobacelara@gmail.com>
2021-09-22 18:05:24 -0300

    Created total and days hooks

--- src/components/CountryCard/index.js
+++ src/components/CountryCard/index.js
@@ -1,9 +1,22 @@
 import "./styles.css";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import React from "react";
 
 const CountryCard = ({ name, data }) => {
-  useEffect(() => {}, [data]);
+  const [total, setTotal] = useState(0);
+  const [newDaily, setNewDaily] = useState([]);
+  const [days, setDays] = useState([]);
+
+  useEffect(() => {
+    const arrangeData = () => {
+      setDays(Object.keys(data).slice(1));
+      setTotal(
+        data[Object.keys(data)[Object.keys(data).length - 1]].toLocaleString()
+      );
+    };
+
+    arrangeData();
+  }, [data]);
 
   return (
     <div className="section__grid-item">
