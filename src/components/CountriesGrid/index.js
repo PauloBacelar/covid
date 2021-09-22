c4fda3fe6cea115bc036479c263a1cdc5c4612cd
Paulo Guilherme Bacelar Andrade <paulobacelara@gmail.com>
2021-09-22 16:53:34 -0300

    Rendered CountryCard component, passing name, cases and deaths data

--- src/components/CountriesGrid/index.js
+++ src/components/CountriesGrid/index.js
@@ -1,15 +1,18 @@
-import React from "react";
 import "./styles.css";
+import React from "react";
+import CountryCard from "../CountryCard";
 
 const CountriesGrid = ({ data, flags }) => {
-  console.log(data);
   return (
     <div className="section__grid">
       {data.map((country) => {
         return (
-          <div className="section__grid-item">
-            <h3>{country.country}</h3>
-          </div>
+          <CountryCard
+            name={country.country}
+            cases={country.timeline.cases}
+            deaths={country.timeline.deaths}
+            key={country.country}
+          />
         );
       })}
     </div>
