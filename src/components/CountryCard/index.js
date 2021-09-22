5d6a16a3299c1a2a4c956b6919538a0eba1ece5e
Paulo Guilherme Bacelar Andrade <paulobacelara@gmail.com>
2021-09-22 16:52:48 -0300

    CountryCard now has access to data and shows country name

--- src/components/CountryCard/index.js
+++ src/components/CountryCard/index.js
@@ -1,7 +1,12 @@
+import "./CountryCard.css";
 import React from "react";
 
-const CountryCard = () => {
-  return <div className="section__grid-item"></div>;
+const CountryCard = ({ name, cases, deaths }) => {
+  return (
+    <div className="section__grid-item">
+      <h3>{name}</h3>
+    </div>
+  );
 };
 
 export default CountryCard;
