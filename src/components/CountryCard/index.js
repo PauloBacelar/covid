56def343b3d75154004ad6d2a04012eb64d7d8dc
Paulo Guilherme Bacelar Andrade <paulobacelara@gmail.com>
2021-09-23 17:03:08 -0300

    Calculated daily new cases/deaths

--- src/components/CountryCard/index.js
+++ src/components/CountryCard/index.js
@@ -3,24 +3,33 @@ import { useEffect, useState } from "react";
 import React from "react";
 
 const CountryCard = ({ name, data }) => {
-  const [total, setTotal] = useState(0);
+  const [total] = useState(
+    data[Object.keys(data)[Object.keys(data).length - 1]]
+  );
   const [newDaily, setNewDaily] = useState([]);
   const [days, setDays] = useState([]);
 
   useEffect(() => {
     const arrangeData = () => {
-      setDays(Object.keys(data).slice(1));
-      setTotal(
-        data[Object.keys(data)[Object.keys(data).length - 1]].toLocaleString()
-      );
+      const days = Object.keys(data).slice(1);
+      setDays(days);
+
+      const newDaily = [];
+      Object.keys(data).forEach((day, i) => {
+        newDaily.push(data[days[i]] - data[day]);
+      });
+
+      setNewDaily(newDaily);
     };
 
     arrangeData();
-  }, [data]);
+  }, []);
 
   return (
     <div className="section__grid-item">
       <h3>{name}</h3>
+
+      <div id="chart"></div>
     </div>
   );
 };
