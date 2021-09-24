32d83296182bebb006e38aa6dd6ea1fef92d5fc6
Paulo Guilherme Bacelar Andrade <paulobacelara@gmail.com>
2021-09-24 08:24:23 -0300

    Rendered chart in parent component

--- src/components/CountryCard/index.js
+++ src/components/CountryCard/index.js
@@ -1,19 +1,17 @@
 import "./styles.css";
 import { useEffect, useState } from "react";
 import React from "react";
+import DataChart from "../DataChart";
 
 const CountryCard = ({ name, data }) => {
   const [total] = useState(
     data[Object.keys(data)[Object.keys(data).length - 1]]
   );
+  const [days, setDays] = useState(Object.keys(data).slice(1));
   const [newDaily, setNewDaily] = useState([]);
-  const [days, setDays] = useState([]);
 
   useEffect(() => {
-    const arrangeData = () => {
-      const days = Object.keys(data).slice(1);
-      setDays(days);
-
+    const getNewDaily = () => {
       const newDaily = [];
       Object.keys(data).forEach((day, i) => {
         newDaily.push(data[days[i]] - data[day]);
@@ -22,14 +20,16 @@ const CountryCard = ({ name, data }) => {
       setNewDaily(newDaily);
     };
 
-    arrangeData();
+    getNewDaily();
   }, []);
 
   return (
     <div className="section__grid-item">
       <h3>{name}</h3>
 
-      <div id="chart"></div>
+      <div id="section__grid-chart">
+        <DataChart xLabels={days} yLabels={newDaily} />
+      </div>
     </div>
   );
 };
