f77496a9c6b091fef843f3c8bfb432974e4b3614
Paulo Guilherme Bacelar Andrade <paulobacelara@gmail.com>
2021-09-26 11:04:39 -0300

    Rendered loader

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
+import "./App.css";
 import Navbar from "./components/Navbar";
 import Section from "./components/Section";
 import { useEffect, useState } from "react";
 import data from "./api/api";
+import Loader from "react-loader-spinner";
 
 function App() {
   const [timeline, setTimeline] = useState([]);
   const [vaccination, setVaccination] = useState([]);
   const [flags, setFlags] = useState([]);
   const [population, setPopulation] = useState([]);
+  const [loaded, setLoaded] = useState(false);
 
   useEffect(() => {
     const loadData = async () => {
       let timeline = await data.getCountriesTimeline();
       setTimeline(timeline);
+
+      setLoaded(true);
 
       let vaccination = await data.getVaccinationData();
       setVaccination(vaccination);
@@ -31,37 +36,44 @@ function App() {
       <header>
         <Navbar />
       </header>
-      <main>
-        <div id="cases">
-          <Section
-            title="Cases by country"
-            timeline={timeline}
-            flags={flags}
-            population={population}
-            type="Cases"
-          />
-        </div>
+      {loaded && (
+        <main>
+          <div id="cases">
+            <Section
+              title="Cases by country"
+              timeline={timeline}
+              flags={flags}
+              population={population}
+              type="Cases"
+            />
+          </div>
 
-        <div id="deaths">
-          <Section
-            title="Deaths by country"
-            timeline={timeline}
-            flags={flags}
-            population={population}
-            type="Deaths"
-          />
-        </div>
+          <div id="deaths">
+            <Section
+              title="Deaths by country"
+              timeline={timeline}
+              flags={flags}
+              population={population}
+              type="Deaths"
+            />
+          </div>
 
-        <div id="vaccination">
-          <Section
-            title="Vaccination by country"
-            timeline={vaccination}
-            flags={flags}
-            population={population}
-            type="Doses"
-          />
-        </div>
-      </main>
+          <div id="vaccination">
+            <Section
+              title="Vaccination by country"
+              timeline={vaccination}
+              flags={flags}
+              population={population}
+              type="Doses"
+            />
+          </div>
+        </main>
+      )}
+      {!loaded && (
+        <main className="loader">
+          <Loader type="TailSpin" color="#6feb7a" height={100} width={100} />
+        </main>
+      )}
     </>
   );
 }
