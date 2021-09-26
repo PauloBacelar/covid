9debe8467af4f260a12d3b7bb1b5450b3673b769
Paulo Guilherme Bacelar Andrade <paulobacelara@gmail.com>
2021-09-26 15:14:51 -0300

    Created Footer component markup

---
+++ src/components/Footer/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./styles.css";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="footer">
+        <p className="footer__sources">All sources on GitHub</p>
+        <p>Made with &lt;3 by Paulo Bacelar</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
